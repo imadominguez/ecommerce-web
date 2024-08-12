419fd2033c7d1dbfa476a55356dd6512e00d5f8a
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-08-12 19:09:52 -0300

    fix: eliminacion de proteccion de ruta a nivel de page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,8 @@
 import { auth } from "@/auth";
 import { LogoutButton } from "@/components/button/logout-button";
 
-import { redirect } from "next/navigation";
-
 export default async function DashboardPage() {
   const session = await auth();
-
-  if (!session) {
-    redirect("/");
-  }
 
   return (
     <div>
