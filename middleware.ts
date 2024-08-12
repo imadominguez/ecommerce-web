ec5983e0c6aa1191449fef063b3e5d5b2f03f583
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-08-12 19:09:29 -0300

    middleware protegiendo rutas privadas

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,27 @@
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
+import { NextResponse } from "next/server";
 
 export const { auth: middleware } = NextAuth(authConfig);
+
+const publicRoutes = ["/", "/login", "/register"];
+
+export default middleware((req) => {
+  const { nextUrl, auth } = req;
+  const isLoggedIn = !!auth?.user;
+  const isAdmin = auth?.user.role === "admin";
+
+  // proteger el dashboard
+  if (!publicRoutes.includes(nextUrl.pathname) && !isLoggedIn) {
+    return NextResponse.redirect(new URL("/login", nextUrl));
+  }
+
+  if (!publicRoutes.includes(nextUrl.pathname) && isLoggedIn && !isAdmin) {
+    return NextResponse.redirect(new URL("/login", nextUrl));
+  }
+
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
