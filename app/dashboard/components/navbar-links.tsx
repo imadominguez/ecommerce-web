6143d3f0670b54d5e0f695b1cd3b34d0b312ab8d
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-08-14 09:48:16 -0300

    feat: dashboard layout and page

---
+++ app/dashboard/components/navbar-links.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Package2 } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+type DashboardLinks = {
+  name: string;
+  href: string;
+};
+
+const NAVBAR_DASHBOARD_LINKS: DashboardLinks[] = [
+  {
+    name: "dashboard",
+    href: "/dashboard",
+  },
+  {
+    name: "orders",
+    href: "/dashboard/orders",
+  },
+  {
+    name: "productos",
+    href: "/dashboard/products",
+  },
+];
+
+export const NavbarLinks = () => {
+  const pathname = usePathname();
+  console.log({ pathname });
+  return (
+    <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+      <Link href="#" className="flex items-center gap-2 text-lg font-semibold md:text-base">
+        <Package2 className="h-6 w-6" />
+        <span className="sr-only">Acme Inc</span>
+      </Link>
+
+      {NAVBAR_DASHBOARD_LINKS.map(({ name, href }) => {
+        return (
+          <Link
+            key={name}
+            className={cn("transition-colors capitalize hover:text-foreground", {
+              "text-foreground": pathname === href,
+              "text-muted-foreground": pathname !== href,
+            })}
+            href={href}
+          >
+            {name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
+};
