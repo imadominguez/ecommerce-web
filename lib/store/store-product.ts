5c2a8635d47427feccc57279613ea07d3f2f97ad
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-08-14 17:29:45 -0300

    feat: add store product

---
+++ lib/store/store-product.ts
@@ -0,0 +1,16 @@
+import { Product } from "@/types/product";
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+interface ProductState {
+  product: Product;
+  addStock: () => void;
+  changeStatus: (status: string) => void;
+}
+
+const useProductStore = create<ProductState>()(
+  devtools((set) => ({
+    product: {},
+    addStock: () => {},
+    changeStatus: (status) => {},
+  }))
+);
