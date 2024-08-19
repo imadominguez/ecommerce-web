f8824da3a81db9e6b22e44c2d6eea34ec2de8ac2
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-08-19 17:11:39 -0300

    feat: new fonts in app

---
+++ config/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Roboto, Roboto_Mono } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+});
