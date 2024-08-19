101160ed2ea9301a1a547d2429860cc11e581f33
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-08-19 17:02:04 -0300

    fix: component logo

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,27 +1,24 @@
-import { RegisterForm } from "@/app/(auth)/register/components/register-form";
-import { buttonVariants } from "@/components/ui/button";
-import Link from "next/link";
+import { RegisterForm } from '@/app/(auth)/register/components/register-form';
+import { Logo } from '@/components/logo';
+import { buttonVariants } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function RegisterPage() {
   return (
     <div className="flex min-h-dvh flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        {/* <Image
-          alt="Your Company"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          className="mx-auto h-10 w-auto"
-          width={1000}
-          height={1000}
-        /> */}
-        <h2 className=" text-center text-2xl font-bold leading-9 tracking-tight">Crea tu cuenta</h2>
+        <Logo />
+        <h2 className="mt-2 text-center text-2xl font-bold leading-9 tracking-tight">
+          Crea tu cuenta
+        </h2>
       </div>
       <RegisterForm />
-      <p className="mt-5 text-center text-sm ">
+      <p className="mt-5 text-center text-sm">
         Ya tenes una cuenta?
         <Link
           href="/login"
           className={buttonVariants({
-            variant: "link",
+            variant: 'link',
           })}
         >
           Iniciar sesion
