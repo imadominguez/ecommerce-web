134acb6dc740e299aebce93996efe7029b18cda4
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-08-19 19:44:57 -0300

    fix: quita de proteccion de ruta a nivel de layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,8 +2,6 @@ import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
 export default async function AuthLayout({ children }: { children: React.ReactNode }) {
-  const session = await auth();
-
-  session && redirect("/");
+  
   return <>{children}</>;
 }
