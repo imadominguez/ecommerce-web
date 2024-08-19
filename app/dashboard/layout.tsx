f087560b76bbefda86dc0c2712622134da9cd016
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-08-19 16:48:40 -0300

    se elimino proteccion de ruta a nivel layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,48 +1,65 @@
-import { auth } from "@/auth";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { CircleUser, Menu, Package2, Search } from "lucide-react";
-import Link from "next/link";
-import { redirect } from "next/navigation";
-import { NavbarLinks } from "./components/navbar-links";
+import Link from 'next/link';
 
-export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
-  const session = await auth();
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { CircleUser, Menu, Package2, Search } from 'lucide-react';
+import { NavbarLinks } from './components/navbar-links';
 
-  if (!session || session.user.role !== "admin") {
-    redirect("/");
-  }
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <header className="sticky max-w-screen-2xl w-full border-b top-0 mx-auto justify-between flex h-16 items-center gap-4  bg-background px-4 md:px-6">
         <NavbarLinks />
         <Sheet>
           <SheetTrigger asChild>
-            <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+            <Button
+              variant="outline"
+              size="icon"
+              className="shrink-0 md:hidden"
+            >
               <Menu className="h-5 w-5" />
               <span className="sr-only">Toggle navigation menu</span>
             </Button>
           </SheetTrigger>
           <SheetContent side="left">
             <nav className="grid gap-6 text-lg font-medium">
-              <Link href="#" className="flex items-center gap-2 text-lg font-semibold">
+              <Link
+                href="#"
+                className="flex items-center gap-2 text-lg font-semibold"
+              >
                 <Package2 className="h-6 w-6" />
                 <span className="sr-only">Acme Inc</span>
               </Link>
               <Link href="#" className="hover:text-foreground">
                 Dashboard
               </Link>
-              <Link href="#" className="text-muted-foreground hover:text-foreground">
+              <Link
+                href="#"
+                className="text-muted-foreground hover:text-foreground"
+              >
                 Orders
               </Link>
-              <Link href="#" className="text-muted-foreground hover:text-foreground">
+              <Link
+                href="#"
+                className="text-muted-foreground hover:text-foreground"
+              >
                 Products
               </Link>
-              <Link href="#" className="text-muted-foreground hover:text-foreground">
+              <Link
+                href="#"
+                className="text-muted-foreground hover:text-foreground"
+              >
                 Customers
               </Link>
-              <Link href="#" className="text-muted-foreground hover:text-foreground">
+              <Link
+                href="#"
+                className="text-muted-foreground hover:text-foreground"
+              >
                 Analytics
               </Link>
             </nav>
