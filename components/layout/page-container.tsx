819ff63f3999bf2964b4eae7ef43050f39507ca0
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-08-19 23:20:52 -0300

    fix: page container classname and children

--- components/layout/page-container.tsx
+++ components/layout/page-container.tsx
@@ -1,5 +1,20 @@
-import React, { PropsWithChildren } from "react";
+import { cn } from '@/lib/utils';
+import React, { PropsWithChildren } from 'react';
 
-export const PageContainer = ({ children }: PropsWithChildren) => {
-  return <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 lg:px-12 container">{children}</main>;
+interface Props {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const PageContainer = ({ children, className }: Props) => {
+  return (
+    <main
+      className={cn(
+        'container flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 lg:px-12',
+        className
+      )}
+    >
+      {children}
+    </main>
+  );
 };
