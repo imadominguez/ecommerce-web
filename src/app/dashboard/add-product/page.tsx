8269c3a5343fb30c33cb4773d994788957bf0cf1
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-08-31 03:26:31 -0300

    fix: styles

--- src/app/dashboard/add-product/page.tsx
+++ src/app/dashboard/add-product/page.tsx
@@ -41,9 +41,9 @@ export default function AddProductPage() {
           </Badge>
           <div className="hidden items-center gap-2 md:ml-auto md:flex">
             <Button variant="outline" size="sm">
-              Discard
+              Descartar
             </Button>
-            <Button size="sm">Save Product</Button>
+            <Button size="sm">Guardar producto</Button>
           </div>
         </div>
         <div className="grid gap-4 md:grid-cols-[1fr_250px] lg:grid-cols-3 lg:gap-8">
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { Button } from '@/components/ui/button';
-import { CircleUser, Menu, Package2 } from 'lucide-react';
+import { Menu, Package2 } from 'lucide-react';
 import { NavbarLinks } from './components/navbar-links';
 import { SearchInput } from '@/components/product/search-input';
 
@@ -67,23 +67,6 @@ export default function DashboardLayout({
         </Sheet>
         <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
           <SearchInput />
-
-          {/* <DropdownMenu>
-          <DropdownMenuTrigger asChild> */}
-          <Button variant="secondary" size="icon" className="rounded-full">
-            <CircleUser className="h-5 w-5" />
-            <span className="sr-only">Toggle user menu</span>
-          </Button>
-          {/* </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Settings</DropdownMenuItem>
-            <DropdownMenuItem>Support</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Logout</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu> */}
         </div>
       </header>
 
