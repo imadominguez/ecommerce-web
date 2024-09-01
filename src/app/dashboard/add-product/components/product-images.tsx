bb6cd975921725ab849952e2b52315adb921745b
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-08-31 21:28:04 -0300

    feat: add images in product store

--- src/app/dashboard/add-product/components/product-images.tsx
+++ src/app/dashboard/add-product/components/product-images.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import {
   Card,
@@ -8,10 +10,26 @@ import {
 } from '@/components/ui/card';
 
 import { Upload } from 'lucide-react';
+import { useProductStore } from '@/store/product/useProductStore';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 const PRODUCT_IMAGE_PLACEHOLDER = '/imgs/placeholder.jpg';
 
 export const ProductImages = () => {
+  const { images, setImages, clearImages } = useProductStore((state) => ({
+    images: state.images,
+    setImages: state.setImages,
+    clearImages: state.clearImages,
+  }));
+
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = Array.from(event.target.files || []);
+    const newImages = files.map((file) => URL.createObjectURL(file));
+    setImages([...images, ...newImages]);
+  };
+
   return (
     <Card className="overflow-hidden" x-chunk="dashboard-07-chunk-4">
       <CardHeader>
@@ -23,36 +41,111 @@ export const ProductImages = () => {
       </CardHeader>
       <CardContent>
         <div className="grid gap-2">
-          <Image
-            alt="Product image"
-            className="aspect-square w-full rounded-md object-cover"
-            height="300"
-            src={PRODUCT_IMAGE_PLACEHOLDER}
-            width="300"
-          />
+          <p>
+            <span className="font-semibold">Nota:</span> Puedes subir hasta 5
+            imágenes.
+          </p>
           <div className="grid grid-cols-3 gap-2">
-            <button>
-              <Image
-                alt="Product image"
-                className="aspect-square w-full rounded-md object-cover"
-                height="84"
-                src={PRODUCT_IMAGE_PLACEHOLDER}
-                width="84"
-              />
-            </button>
-            <button>
-              <Image
-                alt="Product image"
-                className="aspect-square w-full rounded-md object-cover"
-                height="84"
-                src={PRODUCT_IMAGE_PLACEHOLDER}
-                width="84"
-              />
-            </button>
-            <button className="flex aspect-square w-full items-center justify-center rounded-md border border-dashed">
+            {images.length === 0 ? (
+              <>
+                <div className="col-span-3">
+                  <Image
+                    alt="Placeholder"
+                    className="aspect-square w-full rounded-md object-cover"
+                    height={300}
+                    src={PRODUCT_IMAGE_PLACEHOLDER}
+                    width={300}
+                  />
+                </div>
+                <Image
+                  alt="Placeholder"
+                  className="aspect-square w-full rounded-md object-cover"
+                  height={300}
+                  src={PRODUCT_IMAGE_PLACEHOLDER}
+                  width={300}
+                />
+                <Image
+                  alt="Placeholder"
+                  className="aspect-square w-full rounded-md object-cover"
+                  height={300}
+                  src={PRODUCT_IMAGE_PLACEHOLDER}
+                  width={300}
+                />
+              </>
+            ) : images.length === 1 ? (
+              <>
+                <div className="col-span-3">
+                  <Image
+                    alt="Product Image"
+                    className="aspect-square w-full rounded-md object-cover"
+                    height={300}
+                    src={images[0]}
+                    width={300}
+                  />
+                </div>
+                <Image
+                  alt="Placeholder"
+                  className="aspect-square w-full rounded-md object-cover"
+                  height={300}
+                  src={PRODUCT_IMAGE_PLACEHOLDER}
+                  width={300}
+                />
+                <Image
+                  alt="Placeholder"
+                  className="aspect-square w-full rounded-md object-cover"
+                  height={300}
+                  src={PRODUCT_IMAGE_PLACEHOLDER}
+                  width={300}
+                />
+              </>
+            ) : (
+              <>
+                <div className="col-span-3">
+                  <Image
+                    alt="Product Image"
+                    className="aspect-square w-full rounded-md object-cover"
+                    height={300}
+                    src={images[0]}
+                    width={300}
+                  />
+                </div>
+                {images.slice(1).map((image, index) => (
+                  <Image
+                    alt="Product Image"
+                    className="aspect-square w-full rounded-md object-cover"
+                    height={300}
+                    key={index}
+                    src={image}
+                    width={300}
+                  />
+                ))}
+              </>
+            )}
+
+            <Label
+              className="flex aspect-square w-full cursor-pointer items-center justify-center rounded-md border border-dashed"
+              htmlFor="image-upload"
+            >
               <Upload className="h-4 w-4 text-muted-foreground" />
               <span className="sr-only">Upload</span>
-            </button>
+              <Input
+                id="image-upload"
+                accept="image/png"
+                className="hidden"
+                multiple
+                onChange={handleImageUpload}
+                type="file"
+              />
+            </Label>
+          </div>
+          <div className="mt-3">
+            <Button
+              variant={'secondary'}
+              className="w-full"
+              onClick={clearImages}
+            >
+              Limpiar
+            </Button>
           </div>
         </div>
       </CardContent>
--- src/store/product/useProductStore.ts
+++ src/store/product/useProductStore.ts
@@ -9,12 +9,15 @@ interface ProductState {
   category: string;
   isFeatured: boolean;
   status: boolean;
+  images: string[];
   setName: (name: string) => void;
   setDescription: (description: string) => void;
   setInStock: (inStock: number) => void;
   setCategory: (category: string) => void;
   setIsFeatured: (isFeatured: boolean) => void;
   setStatus: (status: boolean) => void;
+  setImages: (images: string[]) => void;
+  clearImages: () => void;
   clearStore: () => void;
 }
 
@@ -27,12 +30,15 @@ export const useProductStore = create<ProductState>()(
       category: '',
       isFeatured: false,
       status: false,
+      images: [],
       setName: (name) => set({ name }),
       setDescription: (description) => set({ description }),
       setInStock: (inStock) => set({ inStock }),
       setCategory: (category) => set({ category }),
       setIsFeatured: (isFeatured) => set({ isFeatured }),
       setStatus: (status) => set({ status }),
+      setImages: (images) => set({ images }),
+      clearImages: () => set({ images: [] }),
       clearStore: () =>
         set({
           name: '',
@@ -41,6 +47,7 @@ export const useProductStore = create<ProductState>()(
           category: '',
           isFeatured: false,
           status: false,
+          images: [],
         }),
     }),
     {
