97fc636ad301053968a282760e979573fd2fd1bb
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-09-05 12:00:04 -0300

    refactor: Update create-product.ts to adjust product price and round to the nearest multiple of 5

--- src/actions/products/create-product.ts
+++ src/actions/products/create-product.ts
@@ -30,6 +30,20 @@ const productSchema = z.object({
   categoryId: z.string(),
 });
 
+function calcularPrecioAjustado(
+  precioBase: number,
+  comision: number,
+  iva?: number
+) {
+  iva = iva || 1.21;
+  const comisionTotal = comision * iva;
+  return precioBase / (1 - comisionTotal);
+}
+
+const redondearMultiploDeCinco = (numero: number) => {
+  return Math.ceil(numero / 5) * 5;
+};
+
 export const createProduct = async (productData: FormData) => {
   // Asegúrate de que formData es un objeto plano
   const data = Object.fromEntries(productData);
@@ -43,20 +57,34 @@ export const createProduct = async (productData: FormData) => {
     };
   }
 
+  // Obtener el producto de la base de datos
   const product = productParsed.data;
-  // product.slug = product.title.toLowerCase().replace(/ /g, '-');
+
+  // Ajustar el precio del producto
+  const precioDeseado = parseFloat(product.price);
+  const comisionMercadoPago = 6.29 / 100; // 6.29%
+  const iva = 1.21; // 21% IVA
+  const precioAjustado = calcularPrecioAjustado(
+    precioDeseado,
+    comisionMercadoPago,
+    iva
+  );
+  // Redondear el precio al múltiplo de 5 más cercano (5, 10, 15, 20, etc.) por ejemplo, 12 -> 15, 17 -> 20
+  const precioRedondeado = redondearMultiploDeCinco(precioAjustado);
 
   try {
     await db.$transaction(async () => {
+      // Separar las etiquetas en un array
       const tagsArray = product.tags
         .split(',')
         .map((tag) => tag.trim().toLowerCase());
 
+      // Crear el producto en la base de datos
       const newProduct = await db.product.create({
         data: {
           ...product,
           color: product.color === '' ? null : (product.color as Color),
-          price: parseFloat(product.price),
+          price: precioRedondeado,
           inStock: parseInt(product.inStock),
           // inDiscount: product.inDiscount === 'true',
           isActive: product.isActive === 'true',
@@ -86,6 +114,7 @@ export const createProduct = async (productData: FormData) => {
         });
 
         try {
+          // Subir las imagenes a cloudinary
           const imagesUploaded = await uploadImages(imageFiles);
 
           // Asegúrate de que se haya subido al menos una imagen
@@ -93,6 +122,7 @@ export const createProduct = async (productData: FormData) => {
             throw new Error('Se produjo un error al cargar las imágenes');
           }
 
+          // Crear los registros de las imagenes en la base de datos
           await db.productImage.createMany({
             data: imagesUploaded.map((url) => ({
               url: url!,
@@ -132,6 +162,7 @@ export const createProduct = async (productData: FormData) => {
       return newProduct;
     });
 
+    // Revalidar las rutas de productos
     revalidatePath('/dashboard/products');
     revalidatePath('/products');
 
@@ -149,8 +180,8 @@ export const createProduct = async (productData: FormData) => {
   }
 };
 
+// Función para subir las imagenes a cloudinary
 async function uploadImages(images: File[]) {
-  // Upload images to cloudinary
   try {
     const uploadPromises = images.map(async (image) => {
       try {
