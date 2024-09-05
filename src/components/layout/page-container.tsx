bf26b0e8942467423b06b3d1643662e80cae116a
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-09-05 16:02:53 -0300

    refactor: Update image loading in PageContainer component

--- src/components/layout/page-container.tsx
+++ src/components/layout/page-container.tsx
@@ -9,7 +9,7 @@ export const PageContainer = ({ children, className }: Props) => {
   return (
     <main
       className={cn(
-        'mx-auto flex w-full max-w-7xl flex-1 flex-col px-4 py-6 sm:px-6 sm:py-8 lg:px-8 lg:py-10',
+        'mx-auto flex w-full max-w-7xl flex-1 flex-col px-4 py-6 sm:px-6 sm:py-8 lg:px-8',
         className
       )}
     >
