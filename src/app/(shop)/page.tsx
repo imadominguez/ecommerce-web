40692094acbe47a5860f32bbcc38aef022ad5d59
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-09-05 16:02:19 -0300

    refactor: Update page.tsx to add "Sobre nosotros" section

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -12,11 +12,36 @@ export default async function Home() {
       {/* <Carrousel images={CARROUSEL_IMAGES_HOME} delay={1000} loop /> */}
       <Carrousel />
 
+      {/* Section sobre nosotros */}
+      <section className="py-16">
+        <div className="container mx-auto grid grid-cols-1 gap-8">
+          <div className="flex flex-col items-center justify-center space-y-4">
+            <h2 className="text-4xl uppercase text-primary">
+              <strong>Sobre nosotros</strong>
+            </h2>
+            <p className="max-w-3xl text-center">
+              Descubre nuestra <strong>plataforma integral</strong> que ofrece{' '}
+              <strong>
+                servicios expertos en calefacción, seguridad y computación
+              </strong>
+              . Desde{' '}
+              <strong>
+                instalaciones de sistemas de calefacción eficientes{' '}
+              </strong>
+              hasta <strong>soluciones de seguridad avanzadas</strong> y{' '}
+              <strong>servicios de informática personalizados</strong>, estamos
+              aquí para cubrir todas tus necesidades. Confía en nosotros para
+              garantizar un hogar seguro, cálido y tecnológicamente avanzado.
+            </p>
+          </div>
+        </div>
+      </section>
+
       <Suspense
         fallback={
           <div className="mx-auto grid w-full max-w-2xl gap-3 py-16 sm:py-16 lg:max-w-none">
             <h2 className="text-2xl font-bold text-gray-900">
-              Productos destacados
+              <strong>Productos destacados</strong>
             </h2>
             <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
               {/* Aquí se mostrarán los productos destacados */}
