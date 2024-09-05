af4252c45c12b45bbf49aadea0c2f7df50081c5d
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-09-05 16:02:46 -0300

    refactor: Update SkeletonTableProduct component to include CardFooter

--- src/components/product/skeleton/skeleton-table-product.tsx
+++ src/components/product/skeleton/skeleton-table-product.tsx
@@ -2,6 +2,7 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
@@ -16,7 +17,7 @@ import {
 
 export const SkeletonTableProduct = () => {
   return (
-    <Card>
+    <Card className="h-full flex-1">
       <CardHeader>
         <CardTitle>Productos</CardTitle>
         <CardDescription>
@@ -39,32 +40,55 @@ export const SkeletonTableProduct = () => {
               </TableHead>
             </TableRow>
           </TableHeader>
-          <TableBody>
+          <TableBody className="h-full flex-1">
             {[...Array(5)].map((_, index) => (
-              <TableRow key={index}>
+              <TableRow key={index} className="h-[101px]">
                 <TableCell>
-                  <div className="mr-2 h-8 w-8 rounded-full bg-gray-200" />
+                  <div className="mr-2 h-16 w-16 animate-pulse rounded-md bg-muted-foreground" />
                 </TableCell>
                 <TableCell>
-                  <div className="h-5 w-40 rounded-md bg-gray-200" />
+                  <div className="h-5 w-40 animate-pulse rounded-md bg-muted-foreground" />
                 </TableCell>
                 <TableCell>
-                  <div className="mt-2 h-4 w-12 rounded-md bg-gray-200" />
+                  <div className="mt-2 h-4 w-12 animate-pulse rounded-md bg-muted-foreground" />
                 </TableCell>
                 <TableCell className="hidden md:table-cell">
-                  <div className="mt-2 h-4 w-12 rounded-md bg-gray-200" />
+                  <div className="mt-2 h-4 w-12 animate-pulse rounded-md bg-muted-foreground" />
                 </TableCell>
                 <TableCell className="hidden md:table-cell">
-                  <div className="mt-2 h-4 w-12 rounded-md bg-gray-200" />
+                  <div className="mt-2 h-4 w-12 animate-pulse rounded-md bg-muted-foreground" />
                 </TableCell>
                 <TableCell>
-                  <div className="mt-2 h-4 w-12 rounded-md bg-gray-200" />
+                  <div className="mt-2 h-4 w-12 animate-pulse rounded-md bg-muted-foreground" />
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </CardContent>
+      <CardFooter className="flex justify-between">
+        <div className="max-w-96 rounded text-xs">
+          <span className="animate-pulse bg-muted-foreground text-muted-foreground">
+            Mostrando{' '}
+          </span>
+          <strong className="animate-pulse bg-muted-foreground text-muted-foreground">
+            12-12{' '}
+          </strong>{' '}
+          <span className="animate-pulse bg-muted-foreground text-muted-foreground">
+            de
+          </span>{' '}
+          <strong className="animate-pulse bg-muted-foreground text-muted-foreground">
+            {' '}
+            12
+          </strong>{' '}
+          <span className="animate-pulse bg-muted-foreground text-muted-foreground">
+            producto
+          </span>
+        </div>
+        <div className="h-10 w-[395px] animate-pulse rounded bg-muted-foreground">
+          &nbsp;
+        </div>
+      </CardFooter>
     </Card>
   );
 };
