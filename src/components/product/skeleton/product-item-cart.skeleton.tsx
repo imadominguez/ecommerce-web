96eb5c9fd237de377a3885b8998faa942dbadf0f
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-09-06 18:39:03 -0300

    Add skeleton component for product item in the shopping cart page

---
+++ src/components/product/skeleton/product-item-cart.skeleton.tsx
@@ -0,0 +1,25 @@
+import { Skeleton } from '@/components/ui/skeleton';
+import React from 'react';
+
+export const ProductItemCartSkeleton = () => {
+  return (
+    <div className="flex animate-pulse border-b py-6">
+      <Skeleton className="h-24 w-24 rounded-md" />
+      <div className="ml-4 flex flex-1 flex-col">
+        <div>
+          <div className="flex justify-between">
+            <Skeleton className="h-4 w-1/3" />
+            <Skeleton className="h-4 w-16" />
+          </div>
+          <Skeleton className="mt-2 h-3 w-1/4" />
+          <Skeleton className="mt-1 h-3 w-1/4" />
+        </div>
+        <div className="mt-4 flex flex-1 items-end justify-between">
+          <Skeleton className="h-8 w-20" />
+          <Skeleton className="h-4 w-16" />
+        </div>
+        <Skeleton className="mt-2 h-3 w-1/5" />
+      </div>
+    </div>
+  );
+};
