b70c5903041ba6122f3aa53cfcb6c4f7fe1f2e1f
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-09-08 15:43:01 -0300

    Add Title component for displaying titles with optional subtitles and custom styling

---
+++ src/components/title.tsx
@@ -0,0 +1,20 @@
+interface Props {
+  title: string;
+  subtitle?: string;
+  className?: string;
+}
+
+export const Title = ({ title, subtitle, className }: Props) => {
+  return (
+    // Contenedor principal del título con posibilidad de aplicar una clase personalizada
+    <div className={`${className ? className : ''} my-3`}>
+      {/* Título principal con estilos de fuente específicos */}
+      <h1 className={`mb-3 text-2xl font-semibold antialiased md:text-3xl`}>
+        {title}
+      </h1>
+
+      {/* Subtítulo opcional con estilos de fuente específicos */}
+      {subtitle && <h3 className="mb-5 text-base md:text-xl">{subtitle}</h3>}
+    </div>
+  );
+};
