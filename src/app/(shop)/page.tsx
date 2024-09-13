07c8ef6f57c4e52745061bf13fd29c8913024e95
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-09-13 02:39:35 -0300

    Refactor UI components for order summary and products in cart

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,7 +1,8 @@
 import { Carrousel } from '@/components/carrousel';
 import { PageContainer } from '@/components/layout/page-container';
-import { ProductsFeatured } from './components/products-featured';
+import { ProductsFeatured } from './_components/products-featured';
 import { Suspense } from 'react';
+import { WhyUs } from './_components/why-us';
 
 export default async function Home() {
   return (
@@ -10,25 +11,29 @@ export default async function Home() {
       <Carrousel />
 
       {/* Section sobre nosotros */}
-      <section className="py-16">
+      <section className="my-10 py-16">
         <div className="container mx-auto grid grid-cols-1 gap-8">
           <div className="flex flex-col items-center justify-center space-y-4">
             <h2 className="text-center text-3xl uppercase text-primary md:text-4xl">
               <strong>Sobre nosotros</strong>
             </h2>
-            <p className="max-w-3xl text-balance md:text-center">
-              Descubre nuestra <strong>plataforma integral</strong> que ofrece{' '}
+            <p className="max-w-3xl text-pretty text-center">
+              Descubre nuestra <strong>plataforma integral</strong>
+              ,especializada en{' '}
+              <strong>servicios de calefacción, seguridad y computación</strong>
+              . Ofrecemos soluciones expertas que van desde la
               <strong>
-                servicios expertos en calefacción, seguridad y computación
+                instalación de sistemas de calefacción eficientes{' '}
               </strong>
-              . Desde{' '}
+              hasta la{' '}
               <strong>
-                instalaciones de sistemas de calefacción eficientes{' '}
-              </strong>
-              hasta <strong>soluciones de seguridad avanzadas</strong> y{' '}
-              <strong>servicios de informática personalizados</strong>, estamos
-              aquí para cubrir todas tus necesidades. Confía en nosotros para
-              garantizar un hogar seguro, cálido y tecnológicamente avanzado.
+                implementación de tecnologías avanzadas de seguridad
+              </strong>{' '}
+              y <strong>servicios informáticos personalizados.</strong> Nos
+              comprometemos a cubrir todas tus necesidades, asegurando un{' '}
+              <strong>hogar seguro, cálido y tecnológicamente avanzado</strong>.
+              Confía en nosotros para brindarte el confort y la tranquilidad que
+              mereces.
             </p>
           </div>
         </div>
@@ -36,25 +41,28 @@ export default async function Home() {
 
       <Suspense
         fallback={
-          <div className="mx-auto grid w-full max-w-2xl gap-3 py-16 sm:py-16 lg:max-w-none">
-            <h2 className="text-2xl font-bold text-gray-900">
-              <strong>Productos destacados</strong>
-            </h2>
-            <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
-              {/* Aquí se mostrarán los productos destacados */}
-              {/* Skeleton */}
-              {Array.from({ length: 4 }).map((_, index) => (
-                <div
-                  key={index}
-                  className="h-[400px] w-full animate-pulse rounded-md bg-muted-foreground/40"
-                />
-              ))}
-            </div>
-          </div>
+          // <div className="mx-auto grid w-full max-w-2xl gap-3 py-16 sm:py-16 lg:max-w-none">
+          //   <h2 className="text-2xl font-bold text-gray-900">
+          //     <strong>Productos destacados</strong>
+          //   </h2>
+          //   <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+          //     {/* Aquí se mostrarán los productos destacados */}
+          //     {/* Skeleton */}
+          //     {Array.from({ length: 4 }).map((_, index) => (
+          //       <div
+          //         key={index}
+          //         className="h-[400px] w-full animate-pulse rounded-md bg-muted-foreground/40"
+          //       />
+          //     ))}
+          //   </div>
+          // </div>
+          <div></div>
         }
       >
         <ProductsFeatured />
       </Suspense>
+
+      <WhyUs />
     </PageContainer>
   );
 }
