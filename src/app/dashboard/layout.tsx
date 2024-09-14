839b472140ede5979a3b683d843861364e8bcdf6
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-09-14 19:41:15 -0300

    Refactor dashboard layout component for improved responsiveness and navigation

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,11 @@
 import Link from 'next/link';
-
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
-import { Button } from '@/components/ui/button';
-import { Menu, Package2 } from 'lucide-react';
+import { Button, buttonVariants } from '@/components/ui/button';
+import { Menu } from 'lucide-react';
 import { NavbarLinks } from './(components)/components/navbar-links';
 import { SearchInput } from '@/components/product/search-input';
 import { ModeToggle } from '@/components/button/toggle-mode-button';
-import { Suspense } from 'react';
+import Image from 'next/image';
 
 export default function DashboardLayout({
   children,
@@ -14,13 +13,37 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted">
-      <header className="sticky top-0 z-50 mx-auto flex h-16 w-full items-center justify-between gap-4 border-b bg-background px-4 md:px-6">
-        <div className="mx-auto flex w-full max-w-7xl items-center justify-between gap-4">
-          <Suspense fallback={<div>Loading...</div>}>
-            <NavbarLinks />
-          </Suspense>
-
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <aside className="hidden border-r bg-muted/40 md:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          {/* logo */}
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <Image
+              src={'/imgs/logo-si.png'}
+              width={120}
+              height={40}
+              alt="logo"
+            />
+          </div>
+          <div className="flex-1">
+            <NavbarLinks className="grid items-start px-2 text-sm font-medium lg:px-4" />
+          </div>
+          <div className="mt-auto p-4">
+            <Link
+              href={'/'}
+              className={buttonVariants({
+                size: 'sm',
+                className: 'w-full',
+                variant: 'default',
+              })}
+            >
+              Ir a la tienda
+            </Link>
+          </div>
+        </div>
+      </aside>
+      <div className="flex flex-col">
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
           <Sheet>
             <SheetTrigger asChild>
               <Button
@@ -29,62 +52,33 @@ export default function DashboardLayout({
                 className="shrink-0 md:hidden"
               >
                 <Menu className="h-5 w-5" />
-                <span className="sr-only">
-                  Abrir el menú de navegación principal
-                </span>
+                <span className="sr-only">Toggle navigation menu</span>
               </Button>
             </SheetTrigger>
-            <SheetContent side="left">
-              <nav className="grid gap-6 text-lg font-medium">
+            <SheetContent side="left" className="flex flex-col">
+              <NavbarLinks className="grid gap-2 text-lg font-medium" />
+
+              <div className="mt-auto">
                 <Link
-                  href="#"
-                  className="flex items-center gap-2 text-lg font-semibold"
+                  href={'/'}
+                  className={buttonVariants({
+                    size: 'sm',
+                    className: 'w-full',
+                    variant: 'default',
+                  })}
                 >
-                  <Package2 className="h-6 w-6" />
-                  <span className="sr-only">Dashboard</span>
+                  Ir a la tienda
                 </Link>
-                <Link href="#" className="hover:text-foreground">
-                  Dashboard
-                </Link>
-                <Link
-                  href="#"
-                  className="text-muted-foreground hover:text-foreground"
-                >
-                  Ordenes
-                </Link>
-                <Link
-                  href="#"
-                  className="text-muted-foreground hover:text-foreground"
-                >
-                  Productos
-                </Link>
-                <Link
-                  href="#"
-                  className="text-muted-foreground hover:text-foreground"
-                >
-                  Usuarios
-                </Link>
-                <Link
-                  href="#"
-                  className="text-muted-foreground hover:text-foreground"
-                >
-                  Analytics
-                </Link>
-              </nav>
+              </div>
             </SheetContent>
           </Sheet>
-          <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-            <Suspense fallback={<div>Loading...</div>}>
-              <SearchInput />
-            </Suspense>
+          <div className="w-full flex-1">
+            <SearchInput />
           </div>
-          <div>
-            <ModeToggle />
-          </div>
-        </div>
-      </header>
-
-      {children}
+          <ModeToggle />
+        </header>
+        {children}
+      </div>
     </div>
   );
 }
