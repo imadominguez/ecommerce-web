b3cb6541d8a94ab4733f0997e0f9d5e7f66f8afd
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-09-18 07:43:00 -0300

    Refactor ButtonFavorite component to remove unused code and update button styles

--- src/components/product/button-favorite.tsx
+++ src/components/product/button-favorite.tsx
@@ -2,7 +2,6 @@
 
 import { Heart } from 'lucide-react';
 import { Button } from '../ui/button';
-import { favoriteProduct } from '@/actions/products/favorite-product';
 
 export const ButtonFavorite = ({
   productId,
@@ -16,14 +15,10 @@ export const ButtonFavorite = ({
     return null;
   }
 
-  const handleFavorite = async () => {
-    await favoriteProduct({ productId, userId });
-  };
   return (
     <Button
       size={'icon'}
       className="absolute right-2 top-2 rounded-full text-white shadow-md"
-      onClick={handleFavorite}
     >
       <Heart className="h-5 w-5" />
     </Button>
