11e067d0ba1a287c122b8640d7f769eb60e2af01
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-09-18 06:49:40 -0300

    Refactor DashboardLink component to use single quotes for string literals and remove unnecessary margin class

--- src/components/navbar/dashboard-link.tsx
+++ src/components/navbar/dashboard-link.tsx
@@ -1,20 +1,24 @@
-import Link from "next/link";
-import React from "react";
-import { buttonVariants } from "@/components/ui/button";
-import { auth } from "@/auth";
+import Link from 'next/link';
+import React from 'react';
+import { buttonVariants } from '@/components/ui/button';
+import { auth } from '@/auth';
 
 export const DashboardLink = async () => {
   const session = await auth();
 
   if (!session) return null;
 
-  if (session?.user.role !== "admin") return null;
+  if (session?.user.role !== 'admin') return null;
 
   return (
-    <div className="ml-1 ">
+    <div className="ml-1">
       <Link
-        href={"/dashboard"}
-        className={buttonVariants({ variant: "outline", size: "sm", className: "w-full mt-2 lg:m-0" })}
+        href={'/dashboard'}
+        className={buttonVariants({
+          variant: 'outline',
+          size: 'sm',
+          className: 'w-full',
+        })}
       >
         Dashboard
       </Link>
