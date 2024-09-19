e270d2641431af11b95e306139918f0f01e19227
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-09-19 09:03:14 -0300

    Refactor address type to include additional user address fields

--- src/types/address.d.ts
+++ src/types/address.d.ts
@@ -1,10 +1,19 @@
-export interface Address {
+import type { Country } from './country';
+
+export interface UserAddress {
   firstName: string;
   lastName: string;
-  address: string;
+  street: string;
+  streetNumber: string;
   address2?: string;
+  isApartment: boolean;
+  floor?: string;
+  apartment?: string;
   postalCode: string;
-  city: string;
-  country: string;
   phone: string;
+  city: string;
+  taxType: string;
+  cuitCuil?: string;
+  businessName?: string;
+  vatCondition?: string;
 }
