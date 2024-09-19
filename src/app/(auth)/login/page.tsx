45b508cdefffe9a9b9f4bd627a1983a0f2ae6524
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-09-19 09:03:24 -0300

    Refactor login page component to remove unused code and link to register page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,22 +1,9 @@
-import Link from 'next/link';
 import { LoginForm } from '@/app/(auth)/login/components/login-form';
-import { buttonVariants } from '@/components/ui/button';
 
 export default function LoginPage() {
   return (
     <div className="flex min-h-[90dvh] flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <LoginForm />
-      <p className="mt-5 text-center text-sm">
-        No tenes una cuenta?
-        <Link
-          href="/register"
-          className={buttonVariants({
-            variant: 'link',
-          })}
-        >
-          Crear cuenta
-        </Link>
-      </p>
     </div>
   );
 }
