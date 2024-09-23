330aa509700e0813c672e29153a2c68b187c1672
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-09-23 17:54:00 -0300

    Refactor seed.ts to use single quotes for string literals and update user passwords

--- src/lib/seed/seed.ts
+++ src/lib/seed/seed.ts
@@ -1,8 +1,8 @@
-import bcryptjs from "bcryptjs";
-import { SeedCountry, countries } from "./seed-countries";
+import bcryptjs from 'bcryptjs';
+import { SeedCountry, countries } from './seed-countries';
 
-type ValidColors = "black" | "magenta" | "blue" | "red" | "yellow" | "cyan"; // Colores válidos para los productos
-type SeedRole = "user" | "admin";
+type ValidColors = 'black' | 'magenta' | 'blue' | 'red' | 'yellow' | 'cyan'; // Colores válidos para los productos
+type SeedRole = 'user' | 'admin';
 
 interface SeedProduct {
   title: string;
@@ -32,52 +32,54 @@ interface SeedData {
 }
 
 export const initialData: SeedData = {
-  categories: ["Cartuchos", "Impresoras", "Accesorios", "Calderas"],
-  brand: ["atomlux", "gneiss"],
+  categories: ['Cartuchos', 'Impresoras', 'Accesorios', 'Calderas'],
+  brand: ['atomlux', 'gneiss'],
 
   products: [
     {
-      title: "Cartucho de tinta HP 63",
-      description: "Cartucho de tinta original HP 63",
+      title: 'Cartucho de tinta HP 63',
+      description: 'Cartucho de tinta original HP 63',
       price: 100,
-      color: "black",
+      color: 'black',
       inStock: 100,
-      slug: "cartucho-de-tinta-hp-63",
-      tags: ["HP", "63", "Tinta"],
-      images: ["placeholder.jpg", "starman_750x750.jpg"],
-      productImage: ["1740176-00-A_0_2000.jpg", "1740176-00-A_1.jpg"],
-      brand: "gneiss",
+      slug: 'cartucho-de-tinta-hp-63',
+      tags: ['HP', '63', 'Tinta'],
+      images: ['placeholder.jpg', 'starman_750x750.jpg'],
+      productImage: ['1740176-00-A_0_2000.jpg', '1740176-00-A_1.jpg'],
+      brand: 'gneiss',
     },
     ...Array.from({ length: 49 }).map((_, i) => {
-      const colors: ValidColors[] = ["magenta", "black", "yellow", "cyan"];
+      const colors: ValidColors[] = ['magenta', 'black', 'yellow', 'cyan'];
       return {
         title: `Cartucho GNEISS ${colors[i % 4].toUpperCase()}`,
         description:
-          "Introducing the Tesla Chill Collection. The Men’s Chill Crew Neck Sweatshirt has a premium, heavyweight exterior and soft fleece interior for comfort in any season. The sweatshirt features a subtle thermoplastic polyurethane T logo on the chest and a Tesla wordmark below the back collar. Made from 60% cotton and 40% recycled polyester.",
+          'Introducing the Tesla Chill Collection. The Men’s Chill Crew Neck Sweatshirt has a premium, heavyweight exterior and soft fleece interior for comfort in any season. The sweatshirt features a subtle thermoplastic polyurethane T logo on the chest and a Tesla wordmark below the back collar. Made from 60% cotton and 40% recycled polyester.',
         price: 75,
         inStock: 7,
         color: colors[i % 4],
-        images: ["1473814-00-A_alt.jpg", "1473809-00-A_alt.jpg"],
-        productImage: ["1740176-00-A_0_2000.jpg", "1740176-00-A_1.jpg"],
-        slug: `Cartucho GNEISS ${colors[i % 4].toUpperCase()}`.split("-").join(),
-        tags: ["cartucho", "Tinta", "HP", "Epson"],
-        brand: "gneiss",
+        images: ['1473814-00-A_alt.jpg', '1473809-00-A_alt.jpg'],
+        productImage: ['1740176-00-A_0_2000.jpg', '1740176-00-A_1.jpg'],
+        slug: `Cartucho GNEISS ${colors[i % 4].toUpperCase()}`
+          .split('-')
+          .join(),
+        tags: ['cartucho', 'Tinta', 'HP', 'Epson'],
+        brand: 'gneiss',
       };
     }),
   ],
 
   users: [
     {
-      email: "user-user@gmail.com",
-      password: bcryptjs.hashSync("123456"),
-      role: "user",
-      name: "User User",
+      email: 'user-user@gmail.com',
+      password: bcryptjs.hashSync('12345678'),
+      role: 'user',
+      name: 'User User',
     },
     {
-      email: "user-admin@gmail.com",
-      password: bcryptjs.hashSync("123456"),
-      role: "admin",
-      name: "User Admin",
+      email: 'user-admin@gmail.com',
+      password: bcryptjs.hashSync('12345678'),
+      role: 'admin',
+      name: 'User Admin',
     },
   ],
 
