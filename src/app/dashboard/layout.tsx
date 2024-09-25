cddfc79f40cb8e53a744e1e167e99d3e472c6d8a
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-09-25 10:24:06 -0300

    Refactor layout component to update styles and add sticky header and sidebar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -14,7 +14,7 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <aside className="hidden border-r bg-muted/40 md:block">
+      <aside className="sticky top-0 hidden h-screen border-r bg-background md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           {/* logo */}
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
@@ -43,7 +43,7 @@ export default function DashboardLayout({
         </div>
       </aside>
       <div className="flex flex-col">
-        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+        <header className="sticky top-0 z-10 flex h-14 items-center gap-4 border-b bg-background px-4 lg:h-[60px] lg:px-6">
           <Sheet>
             <SheetTrigger asChild>
               <Button
