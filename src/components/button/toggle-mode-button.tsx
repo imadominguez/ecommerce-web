cfa1e0261a712623b2dd85f1cb040bf24be2b60a
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-09-25 12:41:39 -0300

    Refactor toggle-mode-button component to add className prop to DropdownMenuContent

--- src/components/button/toggle-mode-button.tsx
+++ src/components/button/toggle-mode-button.tsx
@@ -24,7 +24,7 @@ export function ModeToggle() {
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="space-y-1">
         <DropdownMenuItem
           onClick={() => setTheme('light')}
           className={theme === 'light' ? 'bg-muted font-semibold' : ''}
