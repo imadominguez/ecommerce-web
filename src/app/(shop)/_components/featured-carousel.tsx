b153ec7d63a72707777898a18e7e8c5f0bd007a7
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-09-25 12:39:03 -0300

    Refactor featured-carousel component to use ReusableCarousel and update product card layout

--- src/app/(shop)/_components/featured-carousel.tsx
+++ src/app/(shop)/_components/featured-carousel.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ProductCard } from '@/components/product/product-card';
+import ReusableCarousel from '@/components/reusable-carousel';
 import {
   Carousel,
   CarouselContent,
@@ -8,7 +9,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
-import { Product } from '@/types/product';
+import type { Product } from '@/types/product';
 import Autoplay from 'embla-carousel-autoplay';
 import React from 'react';
 
@@ -18,31 +19,43 @@ interface Props {
 
 export const FeaturedCarousel = ({ productsFeatured }: Props) => {
   return (
-    <Carousel
-      plugins={[
-        Autoplay({
-          delay: 2000,
-          stopOnMouseEnter: true,
-          playOnInit: true,
-        }),
-      ]}
-      opts={{
-        align: 'start',
-        loop: true,
-      }}
-      className="mx-auto w-full max-w-xs md:max-w-2xl lg:max-w-4xl"
+    // <Carousel
+    //   plugins={[
+    //     Autoplay({
+    //       delay: 2000,
+    //       stopOnMouseEnter: true,
+    //       playOnInit: true,
+    //     }),
+    //   ]}
+    //   opts={{
+    //     align: 'start',
+    //     loop: true,
+    //   }}
+    //   className="mx-auto w-full max-w-xs md:max-w-2xl lg:max-w-4xl"
+    // >
+    //   <CarouselContent className="-ml-1">
+    //     {productsFeatured.map((product) => (
+    //       <CarouselItem key={product.id} className="pl-1 md:basis-1/3">
+    //         <div className="p-1">
+    //           <ProductCard product={product} />
+    //         </div>
+    //       </CarouselItem>
+    //     ))}
+    //   </CarouselContent>
+    //   <CarouselPrevious />
+    //   <CarouselNext />
+    // </Carousel>
+    <ReusableCarousel
+      autoplay
+      loop
+      autoplayInterval={2000}
+      className="mx-auto max-w-7xl"
     >
-      <CarouselContent className="-ml-1">
-        {productsFeatured.map((product) => (
-          <CarouselItem key={product.id} className="pl-1 md:basis-1/3">
-            <div className="p-1">
-              <ProductCard product={product} />
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+      {productsFeatured.map((product) => (
+        <CarouselItem key={product.id} className="basis-full pl-4 md:basis-1/4">
+          <ProductCard product={product} className="basis-full md:basis-1/4" />
+        </CarouselItem>
+      ))}
+    </ReusableCarousel>
   );
 };
