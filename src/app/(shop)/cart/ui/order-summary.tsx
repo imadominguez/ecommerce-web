88cdc2c06e7aa7ca38e8e61cd63b570f339b8468
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-10-05 18:38:44 -0300

    Refactor OrderSummary component to include loading skeleton for order summary information and update shipping cost display

--- src/app/(shop)/cart/ui/order-summary.tsx
+++ src/app/(shop)/cart/ui/order-summary.tsx
@@ -2,6 +2,8 @@
 
 import { CustomLinkButton } from '@/components/button/custom-link-button';
 import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { Skeleton } from '@/components/ui/skeleton';
 import { useCartStore } from '@/store/shopping-cart/shopping-cart.store';
 import { currencyFormat } from '@/utils/currencyFormat';
 import React, { useEffect, useState } from 'react';
@@ -10,7 +12,7 @@ export const OrderSummary = () => {
   const [loaded, setLoaded] = useState(false);
 
   // Obtenemos información del carrito de compras desde el store
-  const { itemsInCart, subTotal, total } = useCartStore((state) =>
+  const { itemsInCart, subTotal, total, envio } = useCartStore((state) =>
     state.getSummaryInformation()
   );
 
@@ -21,51 +23,41 @@ export const OrderSummary = () => {
 
   if (!loaded) {
     return (
-      <div className="sticky top-24 lg:col-span-1">
-        <div className="rounded-lg bg-muted p-6">
-          <h2 className="mb-4 text-lg font-medium">Resumen del pedido</h2>
-          <div className="flex justify-between py-2">
-            <span className="opacity-80">Subtotal</span>
-            <span className="font-medium">Cargando...</span>
-          </div>
-          <div className="flex justify-between py-2">
-            <span className="opacity-80">Estimación de envío</span>
-            <span className="font-medium">Cargando...</span>
-          </div>
-          <div className="flex justify-between py-2">
-            <span className="opacity-80">Descuento</span>
-            <span className="font-medium">Cargando...</span>
-          </div>
-          <div className="flex justify-between py-2 font-bold">
-            <span>Importe total</span>
-            <span>Cargando...</span>
-          </div>
-          <Button className="mt-6 w-full" size="lg">
-            Proceder al pago
-          </Button>
+     
+      <div className="mt-8">
+      <h2 className="text-lg font-semibold mb-4">Resumen del pedido</h2>
+      {['Subtotal', 'Envío', 'Descuento', 'Importe total'].map((item, i) => (
+        <div key={i} className="flex justify-between items-center mb-2">
+          <Skeleton className="h-4 w-1/4 bg-muted animate-pulse" />
+          <Skeleton className="h-4 w-1/6 bg-muted animate-pulse" />
         </div>
-      </div>
+      ))}
+        {/* Checkout button skeleton */}
+      <Skeleton className="h-12 w-full mt-6 bg-muted animate-pulse" />
+    </div>
+     
     );
   }
   return (
-    <div className="lg:col-span-1">
-      <div className="sticky top-24 rounded-lg bg-muted p-6">
+    
+      <div className=" rounded-lg">
         <h2 className="mb-4 text-lg font-medium">Resumen del pedido</h2>
         <div className="flex justify-between py-2">
           <span className="opacity-80">Subtotal</span>
           <span className="font-medium">{currencyFormat(subTotal)}</span>
         </div>
         <div className="flex justify-between py-2">
-          <span className="opacity-80">Estimación de envío</span>
-          <span className="font-medium">{currencyFormat(0)}</span>
+          <span className="opacity-80">Envio</span>
+          <span className="font-medium">{currencyFormat(envio)}</span>
         </div>
         <div className="flex justify-between py-2">
           <span className="opacity-80">Descuento</span>
           <span className="font-medium">{currencyFormat(0)}</span>
         </div>
+        <Separator />
         <div className="flex justify-between py-2 font-bold">
           <span>Importe total</span>
-          <span>{currencyFormat(subTotal)}</span>
+          <span>{currencyFormat(subTotal + envio)}</span>
         </div>
         <CustomLinkButton
           href={'/checkout/address'}
@@ -75,6 +67,6 @@ export const OrderSummary = () => {
           Proceder al pago
         </CustomLinkButton>
       </div>
-    </div>
+ 
   );
 };
