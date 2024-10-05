ccaf571f0902e56f12715f33df8966f29adb35bb
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-10-05 18:38:47 -0300

    Refactor ProductsCart component to include loading skeletons for product items and update scroll area

--- src/app/(shop)/cart/ui/products-cart.tsx
+++ src/app/(shop)/cart/ui/products-cart.tsx
@@ -10,15 +10,16 @@ import { Button } from '@/components/ui/button';
 import { useCartStore } from '@/store/shopping-cart/shopping-cart.store';
 import { currencyFormat } from '@/utils/currencyFormat';
 import Link from 'next/link';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { cn } from '@/lib/utils';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export const ProdcutsCart = () => {
   const [loaded, setLoaded] = useState(false);
   const productsInCart = useCartStore((state) => state.cart);
 
   // ?? Si no hay productos en el carrito, redirigimos a la pagina de carrito vacio
-  if (productsInCart.length === 0) {
-    redirect('/empty');
-  }
+
 
   const clearCart = useCartStore((state) => state.clearCart);
   const removeProductFromCart = useCartStore(
@@ -32,21 +33,42 @@ export const ProdcutsCart = () => {
     setLoaded(true);
   }, []);
 
+
+  
+
   // Si el componente no ha cargado aún, mostrar esqueletos de carga
   if (!loaded) {
     return (
-      <div className="lg:col-span-2">
-        {Array.from({ length: 3 }, (_, i) => (
-          <ProductItemCartSkeleton key={i} />
-        ))}
+      <div className="h-[46dvh] flex flex-col justify-start">
+        {[...Array(3)].map((_, i) => (
+        <div key={i} className="flex items-center space-x-4 mb-6">
+          <Skeleton className="h-16 w-16 rounded-md bg-muted animate-pulse" />
+          <div className="space-y-2 flex-1">
+            <Skeleton className="h-4 w-1/4 bg-muted animate-pulse" />
+            <Skeleton className="h-4 w-1/6 bg-muted animate-pulse" />
+            <Skeleton className="h-4 w-1/6 bg-muted animate-pulse" />
+            </div>
+            <div className='flex flex-col space-y-4'>
+              <Skeleton className="h-6 w-16 bg-muted" />
+              <Skeleton className="h-6 w-6 rounded-full bg-muted animate-pulse" />
+            </div>
+        </div>
+      ))}
       </div>
     );
   }
+
+  if (productsInCart.length === 0) {
+    redirect('/empty');
+  }
   return (
-    <div className="scroll overflow-y-scroll pr-4 lg:col-span-2 lg:max-h-[80dvh]">
+    <ScrollArea className={cn("pr-4 ", {
+      'lg:h-[50dvh]': productsInCart.length > 3,
+      'h-full': productsInCart.length <= 3,
+    })}>
       {productsInCart.map((item) => (
         <div key={item.id} className="flex border-b py-2 lg:py-6">
-          <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200 lg:h-32 lg:w-32">
+          <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
             <ProductImage
               src={item.image}
               alt={item.title}
@@ -65,7 +87,7 @@ export const ProdcutsCart = () => {
                 </Link>
                 <Button
                   type="button"
-                  onClick={() => removeProductFromCart(item)}
+                  onClick={() => removeProductFromCart(item.id)}
                   variant="ghost"
                   className="h-fit lg:py-2"
                   size={'icon'}
@@ -79,12 +101,7 @@ export const ProdcutsCart = () => {
             </div>
             <div className="flex flex-1 items-end justify-between text-sm">
               <div className="flex items-center">
-                <QuantitySelector
-                  quantity={item.quantity}
-                  onQuantityChange={(value) =>
-                    updatedProductQuantity(item, value)
-                  }
-                />
+               Cantidad: {item.quantity}
               </div>
 
               <p className="ml-4 text-lg">{currencyFormat(item.price)}</p>
@@ -92,6 +109,6 @@ export const ProdcutsCart = () => {
           </div>
         </div>
       ))}
-    </div>
+    </ScrollArea>
   );
 };
