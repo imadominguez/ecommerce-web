885ab3c583148c4b70604858c445b22d0895dade
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-10-05 19:07:04 -0300

    Refactor OrderSummary component to include environment variable for shipping cost

--- src/app/(shop)/cart/ui/order-summary.tsx
+++ src/app/(shop)/cart/ui/order-summary.tsx
@@ -23,50 +23,48 @@ export const OrderSummary = () => {
 
   if (!loaded) {
     return (
-     
       <div className="mt-8">
-      <h2 className="text-lg font-semibold mb-4">Resumen del pedido</h2>
-      {['Subtotal', 'Envío', 'Descuento', 'Importe total'].map((item, i) => (
-        <div key={i} className="flex justify-between items-center mb-2">
-          <Skeleton className="h-4 w-1/4 bg-muted animate-pulse" />
-          <Skeleton className="h-4 w-1/6 bg-muted animate-pulse" />
-        </div>
-      ))}
+        <h2 className="mb-4 text-lg font-semibold">Resumen del pedido</h2>
+        {['Subtotal', 'Envío', 'Descuento', 'Importe total'].map((item, i) => (
+          <div key={i} className="mb-2 flex items-center justify-between">
+            <Skeleton className="h-4 w-1/4 animate-pulse bg-muted" />
+            <Skeleton className="h-4 w-1/6 animate-pulse bg-muted" />
+          </div>
+        ))}
         {/* Checkout button skeleton */}
-      <Skeleton className="h-12 w-full mt-6 bg-muted animate-pulse" />
-    </div>
-     
+        <Skeleton className="mt-6 h-12 w-full animate-pulse bg-muted" />
+      </div>
     );
   }
   return (
-    
-      <div className=" rounded-lg">
-        <h2 className="mb-4 text-lg font-medium">Resumen del pedido</h2>
-        <div className="flex justify-between py-2">
-          <span className="opacity-80">Subtotal</span>
-          <span className="font-medium">{currencyFormat(subTotal)}</span>
-        </div>
-        <div className="flex justify-between py-2">
-          <span className="opacity-80">Envio</span>
-          <span className="font-medium">{currencyFormat(envio)}</span>
-        </div>
-        <div className="flex justify-between py-2">
-          <span className="opacity-80">Descuento</span>
-          <span className="font-medium">{currencyFormat(0)}</span>
-        </div>
-        <Separator />
-        <div className="flex justify-between py-2 font-bold">
-          <span>Importe total</span>
-          <span>{currencyFormat(subTotal + envio)}</span>
-        </div>
-        <CustomLinkButton
-          href={'/checkout/address'}
-          size={'lg'}
-          className="mt-6 w-full"
-        >
-          Proceder al pago
-        </CustomLinkButton>
+    <div className="rounded-lg">
+      <h2 className="mb-4 text-lg font-medium">Resumen del pedido</h2>
+      <div className="flex justify-between py-2">
+        <span className="opacity-80">Subtotal</span>
+        <span className="font-medium">{currencyFormat(subTotal)}</span>
       </div>
- 
+      <div className="flex justify-between py-2">
+        <span className="opacity-80">Envio</span>
+        <span className="font-medium">
+          {currencyFormat(Number(process.env.NEXT_PUBLIC_ENVIO || 0))}
+        </span>
+      </div>
+      <div className="flex justify-between py-2">
+        <span className="opacity-80">Descuento</span>
+        <span className="font-medium">{currencyFormat(0)}</span>
+      </div>
+      <Separator />
+      <div className="flex justify-between py-2 font-bold">
+        <span>Importe total</span>
+        <span>{currencyFormat(subTotal + envio)}</span>
+      </div>
+      <CustomLinkButton
+        href={'/checkout/address'}
+        size={'lg'}
+        className="mt-6 w-full"
+      >
+        Proceder al pago
+      </CustomLinkButton>
+    </div>
   );
 };
