afc5b16283ed23ab9e339e18d55d05ac131b3eb0
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-10-06 22:21:11 -0300

    Refactor set-order.ts to import Product type as a TypeScript type from '@/types/product'

--- src/actions/orders/set-order.ts
+++ src/actions/orders/set-order.ts
@@ -3,7 +3,7 @@
 import { auth } from '@/auth';
 import { db } from '@/lib/db';
 import type { UserAddress } from '@/types/address';
-import { Product } from '@/types/product';
+import type { Product } from '@/types/product';
 interface ProductToOrder {
   productId: string;
   quantity: number;
