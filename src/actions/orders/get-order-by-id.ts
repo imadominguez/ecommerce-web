cfddf395f15c712c2e13f05ef42cf0d5e04ac7c5
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-10-08 19:01:37 -0300

    Refactor getOrderById function to include additional data in the response

---
+++ src/actions/orders/get-order-by-id.ts
@@ -0,0 +1,58 @@
+'use server';
+
+import { db } from '@/lib/db';
+
+export const getOrderById = async (id: string) => {
+  try {
+    const order = await db.order.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        OrderAddress: true,
+        OrderItem: {
+          select: {
+            price: true,
+            quantity: true,
+            product: {
+              select: {
+                title: true,
+                slug: true,
+
+                images: true,
+              },
+            },
+          },
+        },
+        user: {
+          select: {
+            email: true,
+            name: true,
+          },
+        },
+      },
+    });
+
+    if (!order) {
+      throw new Error('No se encontro la orden');
+    }
+
+    const { user, OrderAddress, OrderItem, ...res } = order;
+
+    return {
+      user,
+      OrderAddress,
+      OrderItem,
+      order: res,
+    };
+  } catch (error: any) {
+    console.log(error.message);
+
+    return {
+      user: {},
+      OrderAddress: {},
+      OrderItem: [],
+      order: null,
+    };
+  }
+};
