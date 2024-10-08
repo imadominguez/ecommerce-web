875742a920cf44d2c4a9a88a1c8b55b05be263bb
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-10-08 16:47:32 -0300

    Refactor button component to add a new 'shop' variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        shop: 'bg-green-800 text-white hover:bg-green-700 dark:bg-green-600 dark:hover:bg-green-700',
       },
       size: {
         default: 'h-10 px-4 py-2',
