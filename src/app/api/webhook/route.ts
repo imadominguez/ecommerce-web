c2e7752db4ab3362144d368322831633210a3f1c
Imanol Dominguez Sanchez <98237962+imadominguez@users.noreply.github.com>
2024-10-11 15:46:27 -0300

    Update route.ts

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,8 +1,16 @@
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
+  const body = await req.json();
   return NextResponse.json(
-    { message: 'Webhook received', body: req.body },
+    { message: 'Webhook received', body },
     { status: 200 }
   );
+
+  //?? Obtener payment_id del body que viene en body.data.id
+
+  // obtener cuerpo de la solicitud a mercado pago y obtener el id que recibo en external_reference
+  //?? url de la solicitud a mercado pago: https://api.mercadopago.com/v1/payments/:id
+
+  //?? obtener el id de la orden que recibo en external_reference
 }
