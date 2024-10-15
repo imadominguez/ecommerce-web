0f9b75b82c09caf96191445501d3e7a3d3c54cf1
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-10-15 11:15:57 -0300

    Refactor webhook route to update order status on payment confirmation

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,8 +1,41 @@
+// import { NextResponse } from 'next/server';
+
+import { db } from '@/lib/db';
+import { revalidatePath } from 'next/cache';
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
-  return NextResponse.json(
-    { message: 'Webhook received', body: req.body },
-    { status: 200 }
+  const body = await req.json();
+
+  const payment_id = body.data.id;
+
+  // Obtenemos la orden a partir del external_reference
+  const response = await fetch(
+    `https://api.mercadopago.com/v1/payments/${payment_id}`,
+    {
+      method: 'GET',
+    }
   );
+
+  const data = await response.json();
+  const order_id = data.external_reference;
+
+  // Actualizamos el isPaid de la orden en la base de datos
+  await db.order.update({
+    where: {
+      id: order_id,
+    },
+    data: {
+      isPaid: true,
+      paidAt: new Date(),
+      updatedAt: new Date(),
+    },
+  });
+
+  revalidatePath(`/orders`);
+  revalidatePath(`/orders/${order_id}`);
+  revalidatePath(`/dashboard/orders`);
+  revalidatePath(`/dashboard/orders/${order_id}`);
+
+  return NextResponse.json({}, { status: 200 });
 }
