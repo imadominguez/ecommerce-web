57a7d84971795c509edf9d6d06ccd78920bc3a28
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-10-27 08:39:04 -0300

    feat: new seed database

--- src/lib/seed/seed-database.ts
+++ src/lib/seed/seed-database.ts
@@ -1,81 +1,223 @@
 import { initialData } from './seed';
 import { db } from '../db';
+import bcrypt from 'bcryptjs';
 
 async function main() {
-  await db.orderAddress.deleteMany();
-  await db.orderItem.deleteMany();
-  await db.order.deleteMany();
-  await db.userAddress.deleteMany();
-  await db.productImage.deleteMany();
-  await db.product.deleteMany();
-  await db.brand.deleteMany();
-  await db.account.deleteMany();
-  await db.verificationToken.deleteMany();
-  await db.user.deleteMany();
-  await db.country.deleteMany();
-  await db.category.deleteMany();
+  // Borrar registros existentes en el orden correcto
+  await db.orderItem.deleteMany(); // Primero borra los artículos de las órdenes
+  await db.orderAddress.deleteMany(); // Luego las direcciones de las órdenes
+  await db.order.deleteMany(); // Luego las órdenes
+  await db.comment.deleteMany(); // Borra los comentarios
+  await db.productImage.deleteMany(); // Luego las imágenes de los productos
+  await db.salesHistory.deleteMany(); // Borra el historial de ventas, si existe
+  await db.userAddress.deleteMany(); // Luego las direcciones de los usuarios
+  await db.user.deleteMany(); // Luego los usuarios
+  await db.product.deleteMany(); // Borra los productos
+  await db.brand.deleteMany(); // Luego las marcas
+  await db.category.deleteMany(); // Luego las categorías
+  await db.country.deleteMany(); // Finalmente los países
+  await db.account.deleteMany(); // Borra cuentas si existen
+  await db.verificationToken.deleteMany(); // Borra tokens de verificación si existen
+
   console.log('✅ Registros borrados');
+
   // Obtén los datos de prueba
   const { brand, countries, products, users, categories } = initialData;
+
   // Crea registros de categorías
-  const categoriesData = categories.map((category) => ({
-    name: category,
-  }));
-  await db.category.createMany({
-    data: categoriesData,
-  });
+  const categoriesData = await Promise.all(
+    categories.map(async (category) => {
+      return await db.category.create({ data: { name: category } });
+    })
+  );
 
-  // Obtiene las categorías desde la base de datos
-  const categoriesDB = await db.category.findMany();
-  // Crea un mapa para asignar nombres de categorías a identificadores de base de datos
+  // Crea registros de marcas
+  const brandData = await Promise.all(
+    brand.map(async (b) => {
+      return await db.brand.create({ data: { name: b } });
+    })
+  );
 
-  // Crea registro de marcas
-  const brandData = brand.map((b) => ({
-    name: b,
-  }));
+  // Crea registros de productos
+  const productsWithIds = await Promise.all(
+    products.map(async (product, index) => {
+      const slug = `${product.title.replace(/\s+/g, '-').toLowerCase()}-${index}`; // Generar slug único
+      const createdProduct = await db.product.create({
+        data: {
+          title: product.title,
+          description: product.description,
+          fullDescription: product.fullDescription,
+          price: product.price,
+          color: product.color,
+          inStock: product.inStock,
+          slug: slug,
+          tags: product.tags,
+          images: product.images ?? [],
+          brandId: brandData[Math.floor(Math.random() * brandData.length)].id,
+          categoryId:
+            categoriesData[Math.floor(Math.random() * categoriesData.length)]
+              .id,
+        },
+      });
 
-  await db.brand.createMany({
-    data: brandData,
-  });
+      // Crea imágenes para el producto
+      for (const image of product.images) {
+        await db.productImage.create({
+          data: {
+            url: image,
+            productId: createdProduct.id,
+          },
+        });
+      }
 
-  const brandDB = await db.brand.findMany();
-
-  // Crea registros de productos con imágenes y categorías asignadas aleatoriamente
-  for (let i = 0; i < products.length; i++) {
-    await db.product.create({
-      data: {
-        title: `Producto ${i + 1}`,
-        description: `Descripción del producto ${i + 1}`,
-        price: Math.floor(Math.random() * 1000),
-        color: 'black',
-        inStock: Math.floor(Math.random() * 100),
-        slug: `producto-${i + 1}`,
-        categoryId:
-          categoriesDB[Math.floor(Math.random() * categoriesDB.length)].id,
-        tags: ['tag1', 'tag2'],
-        images: [...(products[i].images ?? [])],
-        brandId: brandDB[Math.floor(Math.random() * brandDB.length)].id,
-        fullDescription: `Descripción completa del producto ${i + 1}`,
-      },
-    });
-  }
+      return createdProduct; // Devuelve el producto creado con su ID
+    })
+  );
 
   console.log('✅ Productos creados');
 
-  users.forEach(async (user) => {
-    await db.user.create({
-      data: user,
-    });
-  });
+  // Crea usuarios
+  const usersWithIds = await Promise.all(
+    users.map(async (user) => {
+      const hashedPassword = await bcrypt.hash(user.password, 10);
+      return db.user.create({
+        data: {
+          ...user,
+          password: hashedPassword,
+        },
+      });
+    })
+  );
   console.log('✅ Usuarios creados');
 
-  countries.forEach(async (country) => {
-    await db.country.create({
-      data: country,
-    });
-  });
+  // Crea direcciones de usuario
+  /*
+  await Promise.all(
+    usersWithIds.map(async (user, index) => {
+      const countryId = countries[index % countries.length]?.id;
 
-  console.log(' ');
+      if (!countryId) {
+        console.error(`Country ID is not available for index ${index}`);
+        return; // O manejar el error de alguna manera
+      }
+
+      return db.userAddress.create({
+        data: {
+          firstName: user?.name?.split(' ')[0] || '',
+          lastName: user?.name?.split(' ')[1] || '',
+          street: 'Calle Ejemplo',
+          streetNumber: '123',
+          postalCode: '7400',
+          phone: '123456789',
+          city: 'Ciudad Ejemplo',
+          isApartment: false,
+          userId: user.id,
+          countryId: countryId,
+          taxType: '',
+        },
+      });
+    })
+  );
+  console.log('✅ Direcciones de usuario creadas');
+  */
+
+  // Crea órdenes y artículos de orden
+  await Promise.all(
+    usersWithIds.map(async (user) => {
+      const order = await db.order.create({
+        data: {
+          userId: user.id,
+          subTotal: Math.floor(Math.random() * 500) + 100,
+          envio: 10,
+          total: Math.floor(Math.random() * 500) + 110,
+          itemsInOrder: Math.floor(Math.random() * 5) + 1,
+        },
+      });
+
+      // Crea artículos de orden
+      const itemsInOrderPromises = Array.from(
+        { length: order.itemsInOrder },
+        async () => {
+          return db.orderItem.create({
+            data: {
+              quantity: Math.floor(Math.random() * 3) + 1,
+              price: Math.floor(Math.random() * 100) + 10,
+              orderId: order.id,
+              productId:
+                productsWithIds[
+                  Math.floor(Math.random() * productsWithIds.length)
+                ].id,
+            },
+          });
+        }
+      );
+      await Promise.all(itemsInOrderPromises);
+    })
+  );
+  console.log('✅ Órdenes y artículos de orden creados');
+
+  // Crea comentarios
+  await Promise.all(
+    productsWithIds.map(async (product) => {
+      return db.comment.create({
+        data: {
+          content: 'Comentario de ejemplo',
+          productId: product.id,
+          userId:
+            usersWithIds[Math.floor(Math.random() * usersWithIds.length)].id,
+        },
+      });
+    })
+  );
+  console.log('✅ Comentarios creados');
+
+  // Crea precios de envío
+  await db.shippingPrice.create({
+    data: {
+      olavarria: 50,
+      otherCities: 100,
+    },
+  });
+  console.log('✅ Precios de envío creados');
+
+  // Crea historial de ventas
+  const generateRandomSales = async (
+    productId: string,
+    month: number,
+    year: number,
+    quantity: number
+  ): Promise<void> => {
+    await db.salesHistory.create({
+      data: {
+        productId,
+        quantity,
+        date: new Date(`${year}-${month}-01`), // Establecer la fecha del primer día del mes
+      },
+    });
+  };
+
+  const createSalesHistory = async (productsWithIds: { id: string }[]) => {
+    const salesPromises: Promise<void>[] = [];
+    const currentYear = new Date().getFullYear(); // Año actual
+
+    productsWithIds.forEach((product) => {
+      // Generar entre 1 a 5 ventas por producto en diferentes meses
+      const numberOfSales = Math.floor(Math.random() * 5) + 1;
+      for (let i = 0; i < numberOfSales; i++) {
+        const month = Math.floor(Math.random() * 12) + 1; // Mes aleatorio entre 1 y 12
+        const quantity = Math.floor(Math.random() * 10) + 1; // Cantidad aleatoria entre 1 y 10
+        salesPromises.push(
+          generateRandomSales(product.id, month, currentYear, quantity)
+        );
+      }
+    });
+
+    await Promise.all(salesPromises);
+  };
+
+  await createSalesHistory(productsWithIds);
+  console.log('✅ Historial de ventas creado');
+
   console.log('✅ Seed ejecutado con éxito');
 }
 
